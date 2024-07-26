4318de9439bb707f63eae40a4b1d7bf1e2537871
Sérgio Fleury <sergiofdf@gmail.com>
2024-07-25 22:17:16 -0300

    feat: add entidade

---
+++ Domain/Entities/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Contato : EntityBase
+    {
+        public string Nome { get; set; }
+        public string Ddd { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
