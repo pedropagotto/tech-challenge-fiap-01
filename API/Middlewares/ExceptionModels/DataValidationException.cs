436ca1a30127cc95a9ea4a96df2e787868095f0d
Sérgio Fleury <sergiofdf@gmail.com>
2024-07-26 00:18:52 -0300

    feat: add exception middleware

---
+++ API/Middlewares/ExceptionModels/DataValidationException.cs
@@ -0,0 +1,23 @@
+﻿namespace API.Middlewares.Exceptions
+{
+    public class DataValidationException : Exception
+    {
+        public string Code;
+        public string Message;
+        public string? Details;
+        public List<Field>? Fields = new();
+
+        private DataValidationException(string errorCode, string message, string details, List<Field> fields)
+        {
+            Code = errorCode;
+            Message = message;
+            Details = details;
+            Fields.AddRange(fields);
+        }
+
+        public static void Throw(string errorCode, string message, string details, List<Field> fields)
+        {
+            throw new DataValidationException(errorCode, message, details, fields);
+        }
+    }
+}
---
+++ API/Middlewares/ExceptionModels/ServerException.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Middlewares.Exceptions
+{
+    public class ServerException : Exception
+    {
+        public string Code;
+        public string Message;
+
+        public ServerException(string errorCode, string message)
+        {
+            Code = errorCode;
+            Message = message;
+        }
+
+        public static void Throw(string errorCode, string message)
+        {
+            throw new ServerException(errorCode, message);
+        }
+    }
+}
