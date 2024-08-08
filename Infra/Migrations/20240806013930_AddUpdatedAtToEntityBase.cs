345cf5c6a9c97929e944b835776dc1cdf602eb43
pagotto <pedropagotto@outlook.com>
2024-08-07 21:19:27 -0300

    add user seed

--- Infra/Migrations/20240806013930_AddUpdatedAtToEntityBase.cs
+++ Infra/Migrations/20240806013930_AddUpdatedAtToEntityBase.cs
@@ -33,6 +33,11 @@ namespace Infra.Migrations
                 type: "timestamp with time zone",
                 nullable: false,
                 defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.Sql(@"INSERT INTO public.""Authentications"" (""Id"", ""Email"", ""Password"", ""EmailValidated"", ""ChangePassword"", ""Profile"", ""CreatedAt"", ""UpdatedAt"") VALUES (1, 'rogeriosouzax@hotmail.com', 'EC7117851C0E5DBAAD4EFFDB7CD17C050CEA88CB', false, false, 1, '2024-08-03 20:35:30.531728 +00:00', '-infinity')");
+            
+            migrationBuilder.Sql(
+                @"INSERT INTO public.""Users"" (""Id"", ""FirstName"", ""LastName"", ""Email"", ""Cpf"", ""AuthenticationId"", ""CreatedAt"", ""UpdatedAt"") VALUES (1, 'Rogerio', 'da Silva Souza', 'rogeriosouzax@hotmail.com', '38865053836', 1, '2024-08-03 20:35:30.531365 +00:00', '-infinity')");
         }
 
         /// <inheritdoc />
