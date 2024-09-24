1a7c7dcbe4c53e988f0b9f53c2dab2b1986197bc
Sérgio Fleury <sergiofdf@gmail.com>
2024-09-23 23:39:36 -0300

    feat: add primeiro teste integracao banco de dados.

---
+++ Tests/Infra/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests.Infra.Fixtures
+{
+    public class DatabaseFixture : IClassFixture<DockerPostgresFixture>
+    {
+        private readonly DockerPostgresFixture _dockerFixture;
+
+        public DatabaseFixture(DockerPostgresFixture dockerFixture)
+        {
+            _dockerFixture = dockerFixture;
+        }
+    }
+}
