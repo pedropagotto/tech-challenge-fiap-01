d909e539bcc603975d3f9abe0174a56f104755c2
Rogerio Da Silva Souza <brw.rsouza@ifood3rd.com>
2024-09-25 21:00:57 -0300

    feat: integration test new release

---
+++ Tests/IntegrationTests/ApiTechChallengeIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Application.ViewModels.Authentication;
+using Domain.Entities;
+
+namespace Tests.IntegrationTests;
+
+public class ApiTechChallengeIntegrationTests
+{
+    private const string AuthUrl = "api/Authentication/Login";
+    
+    [Fact(DisplayName = "Lista todos contatos")]
+    [Trait("Metodo", "ListAllContacts")]
+    public async Task ListAllContacts()
+    {
+        const string url = "api/Contacts";
+        
+        await using var application = new TechChallengeFiapApp();
+        await TechChallengeFiapMockData.CreateData(application, true);
+
+        var client = await Authenticate(application);
+        
+        var result = await client.GetAsync(url);
+        var users = await result.Content.ReadFromJsonAsync<List<Contact>>();
+        
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        Assert.NotNull(users);
+        Assert.NotEqual(users?.Count, 0);
+    }
+
+    private static async Task<HttpClient> Authenticate(TechChallengeFiapApp application)
+    {
+        var login = new LoginRequestModel
+        {
+            Email = "seuronchi@hotmail.com",
+            Password = "102030"
+        };
+        
+        var client = application.CreateClient();
+        
+        var authResponse = await client.PostAsJsonAsync(AuthUrl, login);
+        var auth = await authResponse.Content.ReadFromJsonAsync<AuthenticationResponseModel>();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + auth!.AccessToken);
+
+        return client;
+    }
+}
---
+++ Tests/IntegrationTests/TechChallengeFiapApp.cs
@@ -0,0 +1,27 @@
+﻿using Infra;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace Tests.IntegrationTests;
+
+public class TechChallengeFiapApp: WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var root = new InMemoryDatabaseRoot();
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("TechChallengeFiapDatabase", root));
+        });
+
+        return base.CreateHost(builder);
+    }
+}
